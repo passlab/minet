8988044db159c2f6384411ec8f602d53928129f3
siddhesh <siddheshsarfare1210@gmail.com>
2020-06-24 23:09:50 -0400

    Initial Commit

---
+++ AndroidApp/app/src/main/java/org/tensorflow/lite/examples/imagesegmentation/BrowseGoogleDrive.kt
@@ -0,0 +1,12 @@
+package org.tensorflow.lite.examples.imagesegmentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BrowseGoogleDrive : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_browse_google_drive)
+    }
+}
